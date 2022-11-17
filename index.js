9d40b4abe65f9ac622e7cad33b469e3d93bfea1f
kelvin kimani <kevohkyme35@gmail.com>
2022-11-17 21:46:55 +0300

    version 8

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ menu.addEventListener("click", function(){
     //console.log(mediaquery.classList.contains("mediaquery"))
     if(mediaquery.classList.contains("show-links")){
         mediaquery.classList.remove("show-links")
+        //menu.setAttribute('aria-expanded', true)
     }else{
         mediaquery.classList.add("show-links")
+        //menu.setAttribute('aria-expanded', false)
     }
     //mediaquery.classList.toggle("show-links")
     mediaquery.style.transition = "0.5s"
